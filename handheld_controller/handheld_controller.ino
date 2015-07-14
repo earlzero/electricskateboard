c86dab8a7ff43b8875e7386da371eaa739448f1a
Nikita Sapozhnikov <earlzero@gmail.com>
2015-07-14 23:49:16 +0300

    Cruise control for handheld controller

--- handheld_controller/handheld_controller.ino
+++ handheld_controller/handheld_controller.ino
@@ -9,12 +9,19 @@ byte address[] = "cntrl1";
 //long address = 0xaabbccddeel;
 RF24 radio(9, 10);
 
+int inPin = 7;
+int prev_value = 0;
+boolean cruise_control = false;
+int cruise_speed = 0;
+int prev_speed = 0;
 void setup() {
+  pinMode(inPin, INPUT);
+  digitalWrite(inPin, HIGH);
   Serial.begin(57600);
   printf_begin();
   radio.begin();
   radio.printDetails();
-  radio.setPALevel(RF24_PA_LOW);
+  radio.setPALevel(RF24_PA_HIGH);
   radio.openWritingPipe(address);
   radio.startListening();
 }
@@ -22,10 +29,18 @@ void setup() {
 void loop() {
   radio.stopListening();
   int data = analogRead(3);
-  boolean timeout = false;
   delay(50);
-  int value = (int)(data * 180.0 / 1023);
-  if (!radio.write(&value, sizeof(int))) {
+  int button_value = digitalRead(inPin);
+  int speed = (int)(data * 180.0 / 1023);
+  if(!button_value) {
+    cruise_speed = max(cruise_speed, speed);
+  } else if(prev_speed != speed) {
+    cruise_speed = 0;
+  }
+  prev_speed = speed;
+  speed = max(cruise_speed, speed);
+  Serial.println(speed);  
+  if (!radio.write(&speed, sizeof(int))) {
     //   Serial.println("fail");
   }
   radio.startListening();
