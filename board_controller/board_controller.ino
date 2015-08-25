9ba24288254d46ba30dd0698340b489736b64c47
Nikita Sapozhnikov <earlzero@gmail.com>
2015-08-26 01:40:50 +0300

    integer overflow fixed

--- board_controller/board_controller.ino
+++ board_controller/board_controller.ino
@@ -10,7 +10,7 @@
 byte address[] = "cntrl1";
 
 RF24 radio(9, 10);
-Servo motor; 
+Servo motor;
 
 
 void setup() {
@@ -21,33 +21,31 @@ void setup() {
   radio.setPALevel(RF24_PA_LOW);
   radio.openReadingPipe(1, address);
   radio.startListening();
-  motor.attach(6);
+  motor.attach(5, 1000, 2000);
   motor.writeMicroseconds(1000);
+  delay(400);
+
 }
 
-int timeouts = 0;
-int max_timeouts = 3;
+long max_delay = 100000;
 int speed = 0;
 void loop() {
 
   boolean timeout = false;
   unsigned long started_waiting_at = micros(); // Set up a timeout period, get the current microseconds
-  while ( ! radio.available() ) { // While nothing is received
-    if (micros() - started_waiting_at > 50000 ) { // If waited longer than 200ms, indicate timeout and exit while loop
+  while ( ! radio.available() ) {
+    if ((micros() - started_waiting_at) > max_delay ) { 
       timeout = true;
-      break;
-    }
+      Serial.println("Fuck");
+      break; 
+    } 
   }
   if (timeout) {
-       timeouts++;
-       if(timeouts > max_timeouts) {
-        speed = 92;
-       }
+    speed = 92;
   } else {
-    timeouts = 0;
     speed = 0;
     radio.read(&speed, sizeof(int));
-    Serial.println(speed);
   }
-      motor.write(speed);
+  Serial.println(speed);
+  motor.write(speed);
 }
